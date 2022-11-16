bf1545d27934101b2703b24ca20e65c59e14c608
blieberman@healthcareanalytics.expert <blieberman@healthcareanalytics.expert>
2022-11-15 19:40:27 -0700

    lab 17 progress

--- src/main/java/com/bhlieberman/codefellowship/repositories/SiteUserRepository.java
+++ src/main/java/com/bhlieberman/codefellowship/repositories/SiteUserRepository.java
@@ -3,6 +3,7 @@ package com.bhlieberman.codefellowship.repositories;
 import com.bhlieberman.codefellowship.models.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
     SiteUser findByUsername(String username);
 }
